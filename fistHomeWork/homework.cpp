53353bf35ea33c46461d5d6be2c45b90549b8482
zhuang <814724008@qq.com>
2023-11-01 15:30:08 +0800

    change

--- fistHomeWork/homework.cpp
+++ fistHomeWork/homework.cpp
@@ -1,7 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+typedef struct node{
+    node* next;
+    int val = 0;
+};
+typedef struct nodeHead{
+    node* node;
+    int num = 0;
+}a,b;
+void insert(int number)
 {
-count<<"sad";
+    cout<<a;
+}
+int main(int argc, char const *argv[])
+{
+    int numbers[10] = {5,7,8,3,1,2,6,7,9,10};
+    for(int i = 0;i<10;i++){
+        insert(numbers[i]);
+    }
+    cout<<"hello";
     return 0;
 }
